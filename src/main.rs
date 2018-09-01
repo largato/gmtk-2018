709b6dce330d8a667dce55249afaa946751e5f29
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2018-09-01 13:03:19 -0300

    Hello ggez
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+extern crate ggez;
+
+use ggez::graphics::{DrawMode, Rect};
+use ggez::*;
+
+struct MainState {
+    pos_x: f32,
+}
+
+impl MainState {
+    fn new(_ctx: &mut Context) -> GameResult<MainState> {
+        let s = MainState { pos_x: 0.0 };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        graphics::rectangle(ctx, DrawMode::Fill, Rect::new(self.pos_x, 50.0, 20.0, 20.0))?;
+        graphics::present(ctx);
+        Ok(())
+    }
+}
+
+pub fn main() {
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
+    let state = &mut MainState::new(ctx).unwrap();
+    event::run(ctx, state).unwrap();
+}
