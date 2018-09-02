1441267282feeeed97874e7035ca263d0689c349
Roger Zanoni <rogerzanoni@gmail.com>
2018-09-02 13:49:46 +0200

    Fix shape rendering and keep player inside the screen

--- src/ship.rs
+++ src/ship.rs
@@ -1,37 +1,64 @@
-use ggez::graphics::{Rect, Color, Mesh, MeshBuilder, Point2};
+use ggez::graphics::{Color, Mesh, MeshBuilder, Point2, Rect};
 use ggez::*;
 
 use actor::*;
 
 pub struct Ship {
-    pub pos: Point2,
     pub color: Color,
     pub shape: Mesh,
     pub horizontal_speed: f32,
     pub vertical_speed: f32,
+    pub bounding_rect: Rect,
 }
 
 impl Actor for Ship {
     fn draw(&mut self, ctx: &mut Context) {
         graphics::set_color(ctx, self.color).unwrap();
-        graphics::draw(ctx, &self.shape, self.pos, 0.0).unwrap();
+        graphics::draw(ctx, &self.shape, self.bounding_rect.point(), 0.0).unwrap();
     }
 }
 
 impl Ship {
-    pub fn build_player(ctx: &mut Context, color: Color, horizontal_speed: f32, vertical_speed: f32, rect: Rect) -> Ship {
+    pub fn build_player(
+        ctx: &mut Context,
+        color: Color,
+        horizontal_speed: f32,
+        vertical_speed: f32,
+        rect: Rect,
+    ) -> Ship {
         Ship {
-            pos: Point2::new(rect.x, rect.y),
             color: color,
             horizontal_speed: horizontal_speed,
             vertical_speed: vertical_speed,
-            shape:  MeshBuilder::new()
-                .line(&[
-                      Point2::new(rect.left(), rect.bottom()),
-                      Point2::new(rect.right(), rect.bottom()),
-                      Point2::new(rect.left() + rect.w / 2.0, rect.top()),
-                      Point2::new(rect.left(), rect.bottom()),
-                ], 4.0).build(ctx).unwrap(),
+            shape: MeshBuilder::new()
+                .line(
+                    &[
+                        Point2::new(0.0, rect.h),
+                        Point2::new(rect.w, rect.h),
+                        Point2::new(rect.w / 2.0, 0.0),
+                        Point2::new(0.0, rect.h),
+                    ],
+                    4.0,
+                )
+                .build(ctx)
+                .unwrap(),
+            bounding_rect: rect,
         }
+    }
+
+    pub fn pos_x(&mut self) -> f32 {
+        self.bounding_rect.x as f32
+    }
+
+    pub fn pos_y(&mut self) -> f32 {
+        self.bounding_rect.y as f32
+    }
+
+    pub fn set_pos_x(&mut self, x: f32) {
+        self.bounding_rect.x = x;
+    }
+
+    pub fn set_pos_y(&mut self, y: f32) {
+        self.bounding_rect.y = y;
     }
 }
